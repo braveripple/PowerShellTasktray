ced75c311b36efdd1ee8802498377b62521ca375
braveripple <braveripple720@gmail.com>
2025-01-30 23:06:20 +0900

    新規作成

---
+++ PSqlEditLauncher/PSqlEditExport.ps1
@@ -0,0 +1,14 @@
+﻿$connect_info_reg = Get-ItemProperty HKCU:\Software\OGAWA\POSTGRESQL\PSqlEdit\CONNECT_INFO\ -Name *
+$count = $connect_info_reg.COUNT
+$connect_info = foreach($i in 0..$($count-1)) {
+    $connect_info_reg |
+        select-object  `
+        @{ Name = "User"; Expression = { $_."USER-$i" } }, `
+        @{ Name = "Password"; Expression = { $_."PASSWD-$i" } }, `
+        @{ Name = "DBName"; Expression = { $_."DBNAME-$i" } }, `
+        @{ Name = "Host"; Expression = { $_."HOST-$i" } }, `
+        @{ Name = "PortNo"; Expression = { $_."PORT-$i" } }, `
+#        @{ Name = "Option"; Expression = { $_."OPTION-$i" } }, `
+        @{ Name = "Memo"; Expression = { $_."CONNECT-NAME-$i" } }
+}
+$connect_info | Export-Csv -LiteralPath "接続先_$(Get-Date -Format FileDateTime).tsv" -Delimiter "`t" -UseQuotes Never
